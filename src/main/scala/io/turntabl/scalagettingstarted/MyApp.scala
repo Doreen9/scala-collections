e2d7d08c05237fd092a32948cc9a61f1b04a3580
Doreen9 <doreen.dodoo@turntabl.io>
2019-11-26 15:44:52 +0000

    scala collections examples

---
+++ src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
@@ -0,0 +1,34 @@
+package io.turntabl.scalagettingstarted
+
+
+object MyApp extends App {
+
+    val details = ("Doreen", "Scala")
+
+    var (name, language) = details
+
+    val favorite = Map(
+        "South Korea" -> "Seoul",
+        "USA" -> "Washington DC",
+        "France" -> "Paris"
+    )
+
+
+   favorite + ("Ghana" -> "Accra", "Russia" -> "Moscow")
+
+   // print(favorite.getOrElse("Norway", "Unknown"))
+
+    val friends = Set("Christiana", "Yaa", "Franklin", "Samuel", "Samuel")
+
+    //print(friends.count(_ == "Samuel"))
+
+    //List("David", "Doreen","Christiana", "Yaa", "Franklin", "Samuel") foreach(println)
+
+   // List("David", "Doreen","Christiana", "Yaa", "Franklin", "Samuel") foreach(n => if(n.length > 6) println(n))
+
+    print(List("David", "Doreen","Christiana", "Yaa", "Franklin", "Samuel") count(_.length > 6))
+
+
+
+
+}
